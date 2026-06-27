751611a896b6e5cac8508bbaed280629d141e91c
addmin-official <addmin.official.idg@gmail.com>
2026-06-28 00:04:15 +0300

    feat: load regulated operations readiness framework

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import './enhancements/ErbilEvidenceUpgrade.css';
+import './enhancements/RegulatoryReadinessUpgrade.css';
 import './enhancements/ErbilEvidenceUpgrade';
+import './enhancements/RegulatoryReadinessUpgrade';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
